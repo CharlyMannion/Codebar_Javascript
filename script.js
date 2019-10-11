0e6c07b4e94c69e4f0c809f384e53091ca8f82c2
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-11 11:27:20 +0100

    creates person variable and refactors functions

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+var person = {
+    first_name: 'Charly',
+    topic: 'Chocolate'
+};
+
 function greeting(person) {
-    return 'Hello ' + person + '!'
+    return 'Hello ' + person.first_name + '!'
 }
 
 function sayHello(person) {
     console.log(greeting(person));
 }
 
-function conversation(person, topic) {
+function conversation(person) {
     sayHello(person);
-    console.log('How are you ' + person + '?');
-    console.log('Do you like ' + topic + '?');
-    console.log('Goodbye ' + person);
+    console.log('How are you ' + person.first_name + '?');
+    console.log('Do you like ' + person.topic + '?');
+    console.log('Goodbye ' + person.first_name);
 }
