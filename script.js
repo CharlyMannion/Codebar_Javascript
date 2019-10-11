8563ea13afa4336f4a463985db47cf4cb9f30092
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-11 11:15:39 +0100

    refactors sayHello to call greeting

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+function greeting(person) {
+    return 'Hello ' + person + '!';
+}
+
 function sayHello(person) {
-    console.log('Hello ' + person + '!');
+    console.log(greeting(person));
 }
 
 function conversation(person) {
     sayHello(person);
     console.log('How are you ' + person + '?');
     console.log('Goodbye ' + person);
-}
-
-function greeting(person) {
-    return 'Hello ' + person + '!';
 }
