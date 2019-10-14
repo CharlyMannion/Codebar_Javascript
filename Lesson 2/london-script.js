4653bfba5b57c72387ce6388e1368db7576e1b5a
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-14 17:49:08 +0100

    creates london variable

--- Lesson 2/london-script.js
+++ Lesson 2/london-script.js
@@ -1,5 +1,21 @@
 // used in london.html
 
+var london = {
+    name: 'London',
+    population: 8308369,
+    tallestBuilding: {
+       name:  'Shard',
+       height: '310m'
+    },
+    numberOfUniversities: 43,
+    averageRent: 1106,
+    dailyTubePassengerJourney: 3500000,
+    olympics: [ 1908, 1948, 2012],
+    updatePopulation: function(newPopulation) {
+      this.population = newPopulation;
+    }
+  };
+
 function listDomElements() {
     var children = document.body.childNodes;
     var i;
@@ -8,3 +24,5 @@ function listDomElements() {
       console.log(children[i]);
     }
   }
+
+  
