961ebc7254b1b9606b855a2008dacf7bb135f2ce
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-14 17:48:12 +0100

    creates a function that displays the dom elememts on click

--- Lesson 2/london-script.js
+++ Lesson 2/london-script.js
@@ -1,2 +1,10 @@
 // used in london.html
 
+function listDomElements() {
+    var children = document.body.childNodes;
+    var i;
+  
+    for (i = 0; i <  children.length; i = i + 1) {
+      console.log(children[i]);
+    }
+  }
