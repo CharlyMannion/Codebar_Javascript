38c2b7f1657407fa3eac814f95370d3552a92a56
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-14 17:25:23 +0100

    deletes animals varibale and for loop

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -1,8 +1,1 @@
 // used in lesson2.html
-var animals = ['dog', 'cat', 'rabbit', 'horse', 'elephant', 'monkey'];
-var i;
-
-for (i = 0; i < animals.length; i ++) {
-    var animal = animals[i];
-    console.log(animal);
-}
