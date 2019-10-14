568cdc413ec2d68d889e5458a3af36b1f97c9d8e
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-14 17:19:41 +0100

    adds animals variable and for loop

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -1,3 +1,8 @@
 // used in lesson2.html
-
+var animals = ['dog', 'cat', 'rabbit', 'horse', 'elephant', 'monkey'];
 var i;
+
+for (i = 0; i < animals.length; i = i +1) {
+    var animal = animals[i];
+    console.log(animal);
+}
